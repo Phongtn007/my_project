55860cc894010f898339ce5be36acf4c625ffc85
Phongtn007 <phontn007@gmail.com>
2025-02-09 16:02:57 +0700

    so5

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,17 +1,14 @@
+
 void main() {
-  Map<String, String> sodienthoai = {
-    'phong': '09374276937',
-    'ngoc': '09327835528',
-    'hoang': '02426976236',
-  };
-  sodienthoai['tuyen'] = '08283562862';
-  sodienthoai['vu'] = '02726583627';
-  sodienthoai.addAll({'dung': '0365852745', 'kien': '05473285485'});
-  sodienthoai.remove('hoang');
-  sodienthoai.forEach(
-    (key, value) {
-      print('$key:$value');
-    },
-  );
-  print(sodienthoai);
+  String day = "monday";
+  switch (day) {
+    case "monday":
+      print("hôm nay là thứ 2");
+      break;
+    case "tuesday":
+      print("hôm nay là thứ 3");
+      break;
+    default:
+      print("một ngày khác");
+  }
 }
